7ed660fe04ad9de01db1688740483a1effd88c88
Nautik <github@nautik.pm>
2024-01-09 15:36:24 +0100

    Remove provider overlay in flake.nix

--- flake.nix
+++ flake.nix
@@ -3,29 +3,18 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs";
 
+
   outputs = { self, nixpkgs }: {
-    devShell.x86_64-linux = let
-      tfoverlay = final: prev: {
-        terraform-providers = prev.terraform-providers // {
-          gandi = prev.terraform-providers.gandi.override {
-            owner = "nautik1";
-            repo = "terraform-provider-gandi";
-            rev = "c38a15f10015d5d14a8982ca334709e241092a93";
-            version = "1.1.1";
-            hash = "sha256-fsCtmwyxkXfOtiZG27VEb010jglK35yr4EynnUWlFog";
-            vendorHash = "sha256-EiTWJ4bw8IwsRTD9Lt28Up2DXH0oVneO2IaO8VqWtkw=";
-          };
-        };
+    devShell.x86_64-linux =
+      let
+        pkgs = import nixpkgs { system = "x86_64-linux"; };
+      in pkgs.mkShell {
+        buildInputs = [
+          (pkgs.opentofu.withPlugins (p: [
+            p.openstack p.gandi p.random
+          ]))
+          pkgs.asciinema
+        ];
       };
-      pkgs = import nixpkgs {
-        system = "x86_64-linux";
-        overlays = [ tfoverlay ];
-      };
-    in pkgs.mkShell {
-      buildInputs = [
-        (pkgs.opentofu.withPlugins (p: [ p.openstack p.gandi p.random ]))
-        pkgs.asciinema
-      ];
-    };
   };
 }
