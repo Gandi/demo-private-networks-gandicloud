db38b86611bc3d6263f8e08cf9d26ee2a39aaf33
Nautik <github@nautik.pm>
2024-01-09 18:14:41 +0100

    Use gandi tf provider compatible with Personal Access Tokens

--- flake.nix
+++ flake.nix
@@ -5,16 +5,26 @@
 
 
   outputs = { self, nixpkgs }: {
-    devShell.x86_64-linux =
-      let
-        pkgs = import nixpkgs { system = "x86_64-linux"; };
-      in pkgs.mkShell {
-        buildInputs = [
-          (pkgs.opentofu.withPlugins (p: [
-            p.openstack p.gandi p.random
-          ]))
-          pkgs.asciinema
-        ];
+    devShell.x86_64-linux = let
+      tfoverlay = final: prev: {
+        terraform-providers = prev.terraform-providers // {
+          gandi = prev.terraform-providers.gandi.override {
+            rev = "c38a15f10015d5d14a8982ca334709e241092a93";
+            version = "2.3.0";
+            hash = "sha256-fsCtmwyxkXfOtiZG27VEb010jglK35yr4EynnUWlFog";
+            vendorHash = "sha256-EiTWJ4bw8IwsRTD9Lt28Up2DXH0oVneO2IaO8VqWtkw=";
+          };
+        };
       };
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        overlays = [ tfoverlay ];
+      };
+    in pkgs.mkShell {
+      buildInputs = [
+        (pkgs.opentofu.withPlugins (p: [ p.openstack p.gandi p.random ]))
+        pkgs.asciinema
+      ];
+    };
   };
 }
