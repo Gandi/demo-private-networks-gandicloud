7f9352456758b0377c934822c682f9516f81464c
Nautik <github@nautik.pm>
2024-01-19 09:55:51 +0100

    Allow providers patch updates

--- versions.tf
+++ versions.tf
@@ -2,15 +2,15 @@ terraform {
   required_providers {
     openstack = {
       source  = "registry.terraform.io/terraform-provider-openstack/openstack"
-      version = ">= 1.28"
+      version = "~> 1.52.1"
     }
     gandi = {
       source  = "registry.terraform.io/go-gandi/gandi"
-      version = "2.3.0"
+      version = "~> 2.3.0"
     }
     random = {
       source = "registry.terraform.io/hashicorp/random"
-      version = "3.5.1"
+      version = "~> 3.5.1"
     }
   }
 }
